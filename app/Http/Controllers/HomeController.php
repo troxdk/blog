8c113866296256196864805d405ebc4c18813aac
Anders Gro Larsen <anders@uptime.dk>
2016-05-26 18:08:03 +0200

    Update HomeController (add subtitle to home view)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', ['subTitle' => 'Turen går Fyn rundt']);
     }
 
     public function gallery()
